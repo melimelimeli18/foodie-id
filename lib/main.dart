5c8154253484e3f67bebe58b26f04533d916ae0f
melimelimeli18 <melisaolivia115@gmail.com>
2025-09-29 08:12:43 +0700

    file setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,34 @@
 import 'package:flutter/material.dart';
 
+// Home - Gatha
+import 'screens/home/home-screen.dart';
+import 'screens/home/onboarding-screen.dart';
+import 'screens/home/promo-one.dart';
+import 'screens/home/promo-tree.dart';
+import 'screens/home/promo-two.dart';
+import 'screens/home/splash-screen.dart';
+
+// Menu - Gebi
+import 'screens/menu/menu-screen.dart';
+import 'screens/menu/menu-main-course.dart';
+import 'screens/menu/menu-snacks.dart';
+
+// Sub Menu - Meli
+// import 'screens/menu/sub-menu/menu-desserts.dart';
+// import 'screens/menu/sub-menu/menu-drinks.dart';
+
+// Checkout - Jeje
+import 'screens/checkout/checkout-page.dart';
+import 'screens/checkout/qris-invoice.dart';
+import 'screens/checkout/qris-page.dart';
+import 'screens/checkout/qris-success.dart';
+
+// Profile - Caca
+import 'screens/profile/profile-screen.dart';
+import 'screens/profile/edit-profile.dart';
+import 'screens/profile/contact-us.dart';
+import 'screens/profile/faq-page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,57 +36,44 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Foodie.id',
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
+      // GANTI DISINI BUAT SCREEN, SESUAI ROUTES
+      initialRoute: '/home',
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Foodie.id",
-          style: TextStyle(
-            fontSize: 32,
-            color: Colors.white,
-          ),
-        ),
-        backgroundColor: Colors.orange[500],
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-        ],
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text("Hello world"),
-              Text("Lorem Ipsum",
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.red,
-                  )),
-              Text("Untar"),
-              Image(image: AssetImage('assets/tree.jpg'))
-            ],
-          ),
-        ),
-      ),
-      // bottomNavigationBar: NavigationBar(),
+      routes: {
+        // Home
+        // '/home': (context) => const HomeScreen(),
+        // '/onboarding': (context) => const OnboardingScreen(),
+        // '/promo-one': (context) => const PromoOne(),
+        // '/promo-two': (context) => const PromoTwo(),
+        // '/promo-tree': (context) => const PromoTree(),
+        // '/splash': (context) => const SplashScreen(),
+
+        // Menu
+        // '/menu': (context) => const MenuScreen(),
+        // '/menu-main-course': (context) => const MenuMainCourse(),
+        // '/menu-snacks': (context) => const MenuSnacks(),
+
+        // Sub Menu
+        // '/menu-desserts': (context) => const MenuDesserts(),
+        // '/menu-drinks': (context) => const MenuDrinks(),
+
+        // Checkout
+        // '/checkout': (context) => const CheckoutPage(),
+        // '/qris-invoice': (context) => const QrisInvoice(),
+        // '/qris-page': (context) => const QrisPage(),
+        // '/qris-success': (context) => const QrisSuccess(),
+
+        // Profile
+        // '/profile': (context) => const ProfileScreen(),
+        // '/edit-profile': (context) => const EditProfile(),
+        // '/contact-us': (context) => const ContactUs(),
+        // '/faq': (context) => const FaqPage(),
+      },
     );
   }
 }
